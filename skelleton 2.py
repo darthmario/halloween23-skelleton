6343cc3b23c127b4fbdd657a01613451a2ffc24d
Nate Craddock <nate@natecraddock.com>
2023-10-22 14:04:51 -0500

    updated to support all servos

--- skelleton 2.py
+++ skelleton 2.py
@@ -6,9 +6,14 @@ from lcd_i2c import LCD
 
 # We will define 3 servos for use across the project
 mouth_servo = Servo(pin_id=1)
+mouth_open = 50
+mouth_closed = 88
 headr_servo = Servo(pin_id=2)
+headr_top = 160
+headr_bottom = 20
 headl_servo = Servo(pin_id=3)
-
+headl_top = 20
+headl_bottom = 160
 # We will have a joystick for moving the head
 # Joystick Setup (only using x & y no button)
 xAxis = ADC(Pin(27))
@@ -51,41 +56,61 @@ mode = 1
 
 # Get and Print the Joystick inputs
 def getJoystick():
-    servoL = .5
-    servoR = .5
+
     xValue = xAxis.read_u16()
     yValue = yAxis.read_u16()
     translatedXValue = math.floor(xValue / 65) / 1000
     translatedYValue = math.floor(yValue / 65) / 1000
     # For this skull mechanic up and down are pure for both servos
     # left and right adjust the influence of up and down for left or right servo
-    servoL = translatedXValue
-    servoR = translatedXValue
-    if(translatedYValue >= 0.5):
-        translatedYValue -= 0.5
-    else:
-        translatedYValue = 0.5 - translatedYValue
+    rServo = 20+(140*translatedXValue)
+    lServo = 180-(140*translatedXValue)
     
-#     headr_servo.write(translatedXValue);
-#     headr_servo.write(translatedYValue);
-    print("Joystick " + str(translatedXValue)+", "+str(translatedYValue))
-    print("servoL " + str(servoL) + " servoR " + str(servoR))
+    
+    if(translatedXValue >= .66):
+        if(translatedYValue >=.66):
+            headr_servo.write(90);
+            headl_servo.write(180);
+        elif(translatedYValue <= .33):
+            headr_servo.write(0);
+            headl_servo.write(90);
+        else:
+            headr_servo.write(0);
+            headl_servo.write(180);
+    elif(translatedXValue <= .33):
+        if(translatedYValue >=.66):
+            headr_servo.write(180);
+            headl_servo.write(90);
+        elif(translatedYValue <= .33):
+            headr_servo.write(90);
+            headl_servo.write(0);
+        else:
+            headr_servo.write(180);
+            headl_servo.write(0);
+    else:
+        headr_servo.write(90);
+        headl_servo.write(90);
+    
+#     headr_servo.write(rServo);
+#     headl_servo.write(lServo);
+    #print("Joystick " + str(translatedXValue)+", "+str(translatedYValue))
+#     print("X " + str(translatedXValue) + " Y " + str(translatedYValue))
     
 def getMouthButton():
     buttonValue = mouth_button.value()
     #print("button" + str(buttonValue))
     if buttonValue == 0:
-        mouth_servo.write(90)
+        mouth_servo.write(mouth_open)
     else:
-        mouth_servo.write(0)
+        mouth_servo.write(mouth_closed)
 
 
 #### Main Program Loop
 while True:
     mode
     if mode == 1:
-#         lcd.print("Hello World")
+#       lcd.print("Hello World")
         getJoystick()
         getMouthButton()
         #temporary sleep to limit what is displayed
-        utime.sleep(0.1)
+#         utime.sleep(0.1)
