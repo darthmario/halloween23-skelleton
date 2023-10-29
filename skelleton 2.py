6c17225721d1c51d064e228f0554c915c1eb57c8
Nate Craddock <nate@natecraddock.com>
2023-10-28 22:26:27 -0500

    adding in some bug fix code as well as the updated diagram

--- skelleton 2.py
+++ skelleton 2.py
@@ -1,13 +1,31 @@
 #this is a more focused setup of the skelleton with a little more structure
-from machine import Pin, ADC, I2C
-import utime, math
+from machine import Pin, ADC, I2C, UART
+import utime, math, time
 from servo import Servo
 from lcd_i2c import LCD
+# Ended up using https://github.com/ShrimpingIt/micropython-dfplayer
+# as it just works
+from dfplayer import Player
+
+# We will have a display to show what we're doing currently
+# library uses https://micropython-i2c-lcd.readthedocs.io/en/latest/EXAMPLES.html
+# PCF8574 on 0x50
+I2C_ADDR = 0x27     # DEC 39, HEX 0x27
+NUM_ROWS = 2
+NUM_COLS = 16
+
+i2c = I2C(0, scl=Pin(21), sda=Pin(20), freq=800000)
+lcd = LCD(addr=I2C_ADDR, cols=NUM_COLS, rows=NUM_ROWS, i2c=i2c)
+lcd.begin()
+lcd.set_cursor(0,0)
+lcd.print("Booting")
+lcd.set_cursor(0,1)
+lcd.print("Setup servos")
 
 # We will define 3 servos for use across the project
 mouth_servo = Servo(pin_id=1)
-mouth_open = 50
-mouth_closed = 88
+mouth_open = 33
+mouth_closed = 68
 headr_servo = Servo(pin_id=2)
 headr_top = 160
 headr_bottom = 20
@@ -18,31 +36,37 @@ headl_bottom = 160
 # Joystick Setup (only using x & y no button)
 xAxis = ADC(Pin(27))
 yAxis = ADC(Pin(26))
+lcd.clear()
+lcd.set_cursor(0,0)
+lcd.print("Booting")
+lcd.set_cursor(0,1)
+lcd.print("Setup joystick")
 
 # We will have a button for opening and closing the mouth manually
 mouth_button = Pin(0, Pin.IN, Pin.PULL_UP)
 
 # We will have a keypad setup for choosing the phrases
 
-# We will have a display to show what we're doing currently
-# library uses https://micropython-i2c-lcd.readthedocs.io/en/latest/EXAMPLES.html
-# PCF8574 on 0x50
-# I2C_ADDR = 0x27     # DEC 39, HEX 0x27
-# NUM_ROWS = 2
-# NUM_COLS = 16
-# 
-# i2c = I2C(0, scl=Pin(17), sda=Pin(16), freq=800000)
-# lcd = LCD(addr=I2C_ADDR, cols=NUM_COLS, rows=NUM_ROWS, i2c=i2c)
-# lcd.begin()
-
+lcd.clear()
+lcd.set_cursor(0,0)
+lcd.print("Booting")
+lcd.set_cursor(0,1)
+lcd.print("Setup mp3 player")
 
 # We will have an mp3 player to play the items
+# df=DFPlayer(uart_id=0,tx_pin_id=16,rx_pin_id=17)
+pico_uart0 = UART(0, baudrate=9600, tx=Pin(16), rx=Pin(17), bits=8, parity=None, stop=1)
+pico_busy = Pin(18)
+player = Player(uart=pico_uart0, busy_pin=pico_busy , volume=1.0)
+player.awaitconfig()
+player.awaitvolume()
+player.play(1,1)
 
 # We will have a library of audio to play for the script
 
 
 ### Three Modes ###
-# 1. Manual - in progress
+# 1. Manual - done
 # 2. Automatic - planned
 # 3. Interactive - planned
 ###
@@ -50,7 +74,18 @@ mouth_button = Pin(0, Pin.IN, Pin.PULL_UP)
 # and a variable to hold which mode we're in
 
 mode = 1
+lcd.clear()
+lcd.set_cursor(0,0)
+lcd.print("Complete")
+lcd.set_cursor(0,1)
+lcd.print("Starting App")
 
+
+lcd.clear()
+lcd.set_cursor(0,0)
+lcd.print("Mode: Manual")
+lcd.set_cursor(0,1)
+lcd.print("Freeform")
 #### Manual Mode ####
 # When the skelleton is in manual mode, the joystick controls the movement and the button opens and closes the mouth
 
@@ -65,7 +100,6 @@ def getJoystick():
     # left and right adjust the influence of up and down for left or right servo
     rServo = 20+(140*translatedXValue)
     lServo = 180-(140*translatedXValue)
-    
     
     if(translatedXValue >= .66):
         if(translatedYValue >=.66):
@@ -91,10 +125,7 @@ def getJoystick():
         headr_servo.write(90);
         headl_servo.write(90);
     
-#     headr_servo.write(rServo);
-#     headl_servo.write(lServo);
-    #print("Joystick " + str(translatedXValue)+", "+str(translatedYValue))
-    print("X " + str(translatedXValue) + " Y " + str(translatedYValue))
+#     print("X " + str(translatedXValue) + " Y " + str(translatedYValue))
     
 def getMouthButton():
     buttonValue = mouth_button.value()
@@ -107,10 +138,11 @@ def getMouthButton():
 
 #### Main Program Loop
 while True:
+    player.volume(0.2)
     mode
     if mode == 1:
-#       lcd.print("Hello World")
         getJoystick()
         getMouthButton()
         #temporary sleep to limit what is displayed
 #         utime.sleep(0.1)
+
